b0dac056f576ca43b2863856a855450334a419f7
Ian Lackey <lackey_i@wustl.edu>
2021-03-30 10:40:13 -0500

    adding repo details stub

---
+++ .github/README.md
@@ -0,0 +1,1 @@
+This site uses GitHub actions for CI/CD
--- src/site/_data/repos.js
+++ src/site/_data/repos.js
@@ -1,8 +1,27 @@
 
 const fetch = require("node-fetch");
 const config = require("./site.json");
+const md = require('markdown-it')();
+const contentBaseUrl = 'https://raw.githubusercontent.com';
 
-module.exports = async function () {
+
+async function loadRepoDetails(data) {
+    const repo = { ...data };
+    repo.homepage = repo.homepage ?? repo.html_url;
+
+    let filename = config.github.projectSummaryFilename;
+    const response = await fetch(`${contentBaseUrl}/${config.github.org}/${repo.name}/${repo.default_branch}/${filename}`);
+
+    if (response.ok) {
+        const text = await response.text();
+        repo.readme = md.render(text);
+    }
+
+    return repo;
+}
+
+
+async function loadRepos() {
     const response = await fetch(`https://api.github.com/orgs/${config.github.org}/repos`, {
         headers: {
             Accept: "application/vnd.github.v3+json"
@@ -12,11 +31,21 @@ module.exports = async function () {
     let output = [];
     if (response.ok) {
         const data = await response.json();
-        output = data;
+        const repos = data.filter(d => config.github.excludedRepos.indexOf(d.id) < 0);
+
+        for (let index = 0; index < repos.length; index++) {
+            const repo = await loadRepoDetails(repos[index]);
+            output.push(repo);
+        }
     }
     else {
-        output = [{ name: 'Could not load projects at this time...', description: JSON.stringify(response) }];
+        output = [{
+            name: 'Could not load projects at this time...',
+            description: JSON.stringify({ response, text: await response.text() })
+        }];
     }
-    return output;
 
+    return output;
 };
+
+module.exports = loadRepos;
